544932265389fe9d93adfeb28cb6465297a4880f
Rifqi Faizal <rifqicode23@gmail.com>
2026-07-26 07:55:40 +0700

    fix: connect mongodb in netlify function

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,14 @@
 import serverless from "serverless-http";
 import app from "../../server/src/app.js";
+import connectDatabase from "../../server/src/config/database.js";
 
-export const handler = serverless(app);
+let isConnected = false;
+
+export const handler = async (event: any, context: any) => {
+  if (!isConnected) {
+    await connectDatabase();
+    isConnected = true;
+  }
+
+  return serverless(app)(event, context);
+};
